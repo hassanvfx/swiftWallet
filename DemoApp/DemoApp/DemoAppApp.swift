588d34e207e1c9c8497eb90af6fbbd0285f26ef7
hassan <hassanvfx@gmail.com>
2024-01-05 19:24:07 -0600

    initial commit

---
+++ DemoApp/DemoApp/DemoAppApp.swift
@@ -0,0 +1,18 @@
+//
+//  DemoAppApp.swift
+//  DemoApp
+//
+//  Created by hassan uriostegui on 8/30/22.
+//
+
+import SwiftWallet
+import SwiftUI
+
+@main
+struct DemoAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# SwiftWallet
+
+This framework was built with the ios-framework  config tool.
+[https://github.com/hassanvfx/ios-framework](https://github.com/hassanvfx/ios-framework)
